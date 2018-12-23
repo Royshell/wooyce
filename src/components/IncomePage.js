d9e8513a49fdfde58e9f89a72e7e56e3ab8b7696
Roy Shell <roy.shell5@gmail.com>
2018-12-23 21:48:40 +0200

    version 1 after build

--- src/components/IncomePage.js
+++ src/components/IncomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cleave from 'cleave.js/react';
 import { connect } from 'react-redux';
 import { addIcomeData } from '../actions/actions';
 import PageNavigator from './PageNavigator';
