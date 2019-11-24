0b1357c7b490e0beda569340ccadee3329c0f8e2
Roy Shell <roy.shell5@gmail.com>
2019-11-24 23:08:42 +0200

    Added all

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -24,27 +24,30 @@ class HomePage extends Component {
     });
   };
   onStop = async(recordedBlob) => {
-    console.log('recordedBlob is: ', recordedBlob);
     const wavFile = new File([recordedBlob.blob], 'record.wav', {type: 'audio/wav; codecs=MS_PCM', lastModified: Date.now()});
-    //const wavFile = recordedBlob.blob;
+
     try {
-      await sendFile(wavFile);
-      const grpahDatas = await getGraphData();
+      await sendFile(wavFile);    
+      const grpahDatas = await getGraphData();  
+
+      const graphInfo = grpahDatas[grpahDatas.length - 1].harmony;
+      const sortedGraph = [];
+      graphInfo.sort((a,b) => b.strength - a.strength);   
+
+      graphInfo.forEach(( graphData, index ) => { 
+        index % 2 === 0 ? sortedGraph.push({...graphData}) : sortedGraph.unshift({...graphData});
+      });
+      this.setState({ options: this.getOptions(sortedGraph)});
+      
     } catch(err) {
       console.log(err);
     }
-
-    const graphInfo = grpahDatas[grpahDatas.length - 1];
-    //this.setState({ graphData: graphInfo });
-   this.setState({ options: this.getOptions(graphInfo)});
-   console.log(this.state.options);
   };
   componentDidMount = () => {
     login();
   };
   getOptions = (dataInfo) => {
     const dataPoints = dataInfo.map( ({tune, strength}) => {return { label: tune, y: strength} } );
-    console.log(dataPoints);
     const data = [];
     const dataObject = {
       type: "column", 
